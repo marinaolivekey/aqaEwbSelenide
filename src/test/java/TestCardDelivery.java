02fa187b460b4782839d1ced7ad408867ca178e0
Marina Kononova <marinakononova@mail.ru>
2024-05-21 15:47:59 +0400

    Test error

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -1,3 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class TestCardDelivery {
     //./gradlew clean test -Dselenide.headless=true
+
+    @Test
+    void shouldTestHappyPath() {
+        // java -jar app-card-delivery.jar
+        open("http://localhost:9999");
+
+//        open("http://localhost:9999");
+//        SelenideElement form = $("[data-test-id=callback-form]");
+//        form.$("[data-test-id=name] input").setValue("Василий");
+//        form.$("[data-test-id=phone] input").setValue("+79270000000");
+//        form.$("[data-test-id=agreement]").click();
+//        form.$("[data-test-id=submit]").click();
+//        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+
+    }
 }
