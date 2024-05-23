10e2e5405c1f87aee144ce39ad7335426b78bb4a
Marina Kononova <marinakononova@mail.ru>
2024-05-23 21:22:04 +0400

    united task 1 and task 2

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -1,5 +1,4 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -9,46 +8,21 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 
 public class TestCardDelivery {
-    //./gradlew clean test -Dselenide.headless=true
-
     private String generateDate(long addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
 
     @Test
-    public void shouldTestHappyPath() {
-        // java -jar app-card-delivery.jar
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Пе");
-        $$(".menu-item__control").findBy(Condition.text("Пермь")).click();
-        String planningDate = generateDate(7,"dd.MM.yyyy");
-        // $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").click();
-        if (!generateDate(3,"MM").equals(generateDate(7,"MM"))){
-            $(".calendar__arrow_direction_right").click();
-        }
-        $$("[data-day]").findBy(Condition.text(generateDate(7,"d"))).click();
-        $("[data-test-id='name'] input").setValue("Перов-Водкин Коля");
-        $("[data-test-id='phone'] input").setValue("+79001112233");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на "+ planningDate));
-    }
-}
-
-/*
-@Test
-    public void shouldTestHappyPath() {
-        // java -jar app-card-delivery.jar
-        open("http://localhost:9999");
+    public void shouldTestFormSuccessfully() {
         $("[data-test-id='city'] input").setValue("Пермь");
-        String planningDate = generateDate(4,"dd.MM.yyyy");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Перов-Водкин Коля");
@@ -57,5 +31,27 @@ public class TestCardDelivery {
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на "+ planningDate));
- */
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+    }
+
+
+    @Test
+    public void shouldTestSuccessfullySevenDaysCityFromMenu() {
+        $("[data-test-id='city'] input").setValue("Пе");
+        $$(".menu-item__control").findBy(Condition.text("Пермь")).click();
+        String planningDate = generateDate(7, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").click();
+        if (!generateDate(3, "MM").equals(generateDate(7, "MM"))) {
+            $(".calendar__arrow_direction_right").click();
+        }
+        $$("[data-day]").findBy(Condition.text(generateDate(7, "d"))).click();
+        $("[data-test-id='name'] input").setValue("Перов-Водкин Коля");
+        $("[data-test-id='phone'] input").setValue("+79001112233");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+    }
+}
+
