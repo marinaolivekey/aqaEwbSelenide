535f7bffe4fe6ab86106814507a2ad37e11e0c2b
Marina Kononova <marinakononova@mail.ru>
2024-05-23 13:12:49 +0400

    1 Task done

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -1,17 +1,42 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
 public class TestCardDelivery {
     //./gradlew clean test -Dselenide.headless=true
 
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
-    void shouldTestHappyPath() {
+    public void shouldTestHappyPath() {
         // java -jar app-card-delivery.jar
         open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Пермь");
+        String planningDate = generateDate(4,"dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Перов-Водкин Коля");
+        $("[data-test-id='phone'] input").setValue("+79001112233");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на "+ planningDate));
+
 
 //        open("http://localhost:9999");
-//        SelenideElement form = $("[data-test-id=callback-form]");
+//        SelenideElement form = $("form");
 //        form.$("[data-test-id=name] input").setValue("Василий");
 //        form.$("[data-test-id=phone] input").setValue("+79270000000");
 //        form.$("[data-test-id=agreement]").click();
